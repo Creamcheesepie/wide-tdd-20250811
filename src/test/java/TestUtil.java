724703932f3db5364c770a47b01be70de48a1773
khy <gnldbs1004@naver.com>
2025-08-11 12:21:56 +0900

    출력 내부로 돌리고 확인 후 외부로 다시 설정 완료

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,8 +1,30 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
+    private static PrintStream originalOut;
+    private static PrintStream currentOut;
 
     public static Scanner genScanner(String input) {
         return new Scanner(input);
     }
+
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        originalOut = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        currentOut = new PrintStream(outputStream);
+        System.setOut(currentOut);
+
+        return outputStream;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream outputStream) throws IOException {
+        System.setOut(originalOut);
+        currentOut.close();
+    }
+
 }
